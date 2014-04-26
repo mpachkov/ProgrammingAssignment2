ddf98d03105facca1129bf58a325b5ec332b6234
Mikhail Pachkov <pachkov@mikhails-macbook-pro.fritz.box>
2014-04-27 00:54:16 +0200

    coments updated

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+
+## for non-square matrices we will use ginv function
+## from MASS library to get inverse
+library(MASS)
+
 ## implements new object which can cache calculated
 ## inverse of a matrix. Once calculated the inverse is
 ## kept in memory and might be used at any moment without
 ## new calculation.
 
-## for non-square matrices we will use ginv function to get inverse
-library(MASS)
-
 makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
+    i <- NULL # i keeps inverse of original matrix
+    # object methods definitions
     set <- function(y) {
-        x <<- y
+        x <<- y # original matrix
         i <<- NULL
     }
     get <- function() x
@@ -25,16 +27,24 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns inverse for cached matrix.
+## If cached matrix does not contain its inverse
+## than the function calculates and saves it to the object.
+## If matrix iverse was already calculated than cached
+## inverse is returned.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
     i <- x$getinverse()
+    # if inverse is cached return it without recalculation
     if(!is.null(i)) {
         message("getting cached data")
         return(i)
     }
     data <- x$get()
+    
+    ## if matrix is square use solve()
+    ## else use ginv() which can work with nonsquare matrices
     dataDim <- dim(data)
     if(dataDim[1] == dataDim[2]) {
         i <- solve(data)
